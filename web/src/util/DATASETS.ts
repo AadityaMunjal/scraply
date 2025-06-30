bdda8c87e6512644c1e1a342677649f2aa1ba64c
AadityaMunjal <aadityamunjal06@gmail.com>
2025-06-29 23:41:49 -0400

    add kind & shape field in datasets

--- web/src/util/DATASETS.ts
+++ web/src/util/DATASETS.ts
@@ -4,31 +4,39 @@ const LAYERS: Dataset[] = [
   {
     label: "Pima Diabetes",
     inputName: "pima",
+    kind: "classification",
+    shape: [8, 1],
   },
   {
     label: "MNIST",
     inputName: "MNIST",
+    kind: "classification",
+    shape: [784, 10],
   },
   {
     label: "FashionMNIST",
     inputName: "FashionMNIST",
+    kind: "classification",
+    shape: [784, 10],
   },
   {
     label: "CIFAR-10",
     inputName: "CIFAR10",
+    kind: "classification",
+    shape: [3072, 10],
   },
 ];
 
-const TRANSFORMERS: Dataset[] = [
-  {
-    label: "Alice in Wonderland",
-    inputName: "alice",
-  },
-  {
-    label: "Shakespeare",
-    inputName: "shakespeare",
-  },
-];
+// const TRANSFORMERS: Dataset[] = [
+//   {
+//     label: "Alice in Wonderland",
+//     inputName: "alice",
+//   },
+//   {
+//     label: "Shakespeare",
+//     inputName: "shakespeare",
+//   },
+// ];
 
 const DATASETS = LAYERS;
 
