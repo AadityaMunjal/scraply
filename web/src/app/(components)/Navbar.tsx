b710dcceac102f840f608f2dfb91b58dafe8b91d
AadityaMunjal <aadityamunjal06@gmail.com>
2025-07-05 22:12:19 -0400

    remove demo button

--- web/src/app/(components)/Navbar.tsx
+++ web/src/app/(components)/Navbar.tsx
@@ -10,12 +10,12 @@ const Navbar = () => {
         <div className="mx-4 py-4 pr-7 font-semibold">scraply</div>
       </div>
       <div className="flex">
-        <button
+        {/* <button
           className="relative mx-4 my-auto h-4/5 rounded-md bg-blue-500 px-4 hover:bg-blue-600"
           onClick={() => setIsDemoing(true)}
         >
           Demo
-        </button>
+        </button> */}
       </div>
     </div>
   );
