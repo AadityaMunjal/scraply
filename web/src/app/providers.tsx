11dac28f17fff7896fec4835f743a2572f5ac732
AadityaMunjal <aadityamunjal06@gmail.com>
2025-07-10 17:15:36 -0400

    posthog setup

--- web/src/app/providers.tsx
+++ web/src/app/providers.tsx
@@ -1,8 +1,23 @@
 "use client";
 import { QueryClientProvider } from "@tanstack/react-query";
-// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { queryClient } from "~/util/queryClient";
 import { DemoProvider } from "~/state/DemoContext";
+import posthog from "posthog-js";
+import { PostHogProvider as PHProvider } from "posthog-js/react";
+import { useEffect } from "react";
+
+export function PostHogProvider({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
+      api_host:
+        process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
+      person_profiles: "always",
+      defaults: "2025-05-24",
+    });
+  }, []);
+
+  return <PHProvider client={posthog}>{children}</PHProvider>;
+}
 
 interface ProvidersProps {
   children: React.ReactNode;
@@ -10,8 +25,10 @@ interface ProvidersProps {
 
 export function Providers({ children }: ProvidersProps) {
   return (
-    <DemoProvider>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    </DemoProvider>
+    <PostHogProvider>
+      <QueryClientProvider client={queryClient}>
+        <DemoProvider>{children}</DemoProvider>
+      </QueryClientProvider>
+    </PostHogProvider>
   );
 }
