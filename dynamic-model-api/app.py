fad1fef5ff99f2725c0b8edc3069ffe1688bec5f
mehekn <mehek.niwas@gmail.com>
2025-07-21 16:36:15 -0400

    fixed werzeug web server error for docker image

--- dynamic-model-api/app.py
+++ dynamic-model-api/app.py
@@ -347,4 +347,4 @@ def train_stream():
 
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True, host="0.0.0.0", port=5000)
+    socketio.run(app, debug=True, host="0.0.0.0", port=5000, allow_unsafe_werkzeug=True)
