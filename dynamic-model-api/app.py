13fd02e0185c60d1e45b53bf43e722deb9709f75
Alan Jiang <74510449+Oajoeawfjo-Jioa@users.noreply.github.com>
2025-08-12 05:34:46 -0400

    update

--- dynamic-model-api/app.py
+++ dynamic-model-api/app.py
@@ -20,10 +20,10 @@ import threading
 
 
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:3000", "https://scraply-prod.vercel.app"])
+CORS(app, origins=["http://localhost:3000", "https://scraply-prod.vercel.app", "http://34.237.181.35:5000"])
 socketio = SocketIO(
     app,
-    cors_allowed_origins=["http://localhost:3000", "https://scraply-prod.vercel.app"],
+    cors_allowed_origins=["http://localhost:3000", "https://scraply-prod.vercel.app", "http://34.237.181.35:5000"],
 )
 
 
